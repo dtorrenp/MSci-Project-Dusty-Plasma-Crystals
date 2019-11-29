a8142b1e2513f73dc1390b640690c181acf99b4a
Daniel Torren Peraire <dt1716@ic.ac.uk>
2019-11-29 13:57:12 +0000

    i typed 3 characters

--- MSci_project.cpp
+++ MSci_project.cpp
@@ -103,7 +103,7 @@ class Dust_grain{
     double charge;
 
     Dust_grain(double m , double grain_r , double q, double time_init):mass(m),grain_R(grain_r),charge(q),a_c{0,0,0},time_list_dust{time_init},wake_charge(abs(q)*wake_charge_multiplier),v_i_z(0),
-    generator(std::default_random_engine(clock())),dist(std::normal_distribution<double>(0.0,0.2))
+    generator(std::default_random_engine(clock())),dist(std::normal_distribution<double>(0.0,(k_b*T_i)/m))
     {
         prod_W_vec();        
     }
